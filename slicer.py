cbdf11a8a8d705cabcb7a43b793c1961dc2229c0
John Wiggins <jwiggins@enthought.com>
2014-07-04 23:18:39 -0500

    Parameter-driven contouring!

--- slicer.py
+++ slicer.py
@@ -1,76 +1,53 @@
 import argparse
+import json
 
 import numpy as np
 from reportlab.lib.units import mm, inch
 from reportlab.pdfgen.canvas import Canvas
 from skimage import measure
 import tables
+
 from kiva.pdf import GraphicsContext as PdfGraphicsContext
 
-PARAMS = {
-    'bone': {
-        'contour_index': [None] * 60,
-        'isovalue': 0.425,
-        'last_slice': 57,
-    },
-    'skin': {
-        'contour_index': [0] * 52 + [1] + [2] * 6 + [0],
-        'isovalue': 0.17,
-        'last_slice': 59,
-    },
-}
-TISSUE = 'skin'
-params = PARAMS[TISSUE]
-
-# Slice Thickness = 3.0 mm
-THICKNESS_SCALE = 2.3622/3.0  # 0.093" thick material
-# Pixel Spacing DS: ['0.48828125', '0.48828125']
-PIXEL_SPACING = 0.48828125
-pixels_to_points = lambda con: con * PIXEL_SPACING * mm * THICKNESS_SCALE
-box_area = lambda box: box[2] * box[3]
-
-LASER_HAIRLINE = 0.001 * inch
-PDF_DPI = 72.0
-CANVAS_W = 24.0 * PDF_DPI
-CANVAS_H = 18.0 * PDF_DPI
-
-MACHINE_BOX_0 = [0, 190, 120, 322]
-MACHINE_BOX_1 = [392, 190, 120, 322]
-SLICE_BOX = [0, 0, 512, 512]
-AXIS_CENTER = (238, 346)
-
-# Post Radius (1/16" welding rod)
-POST_RADIUS = 0.889  # mm
-POST0_CENTER = (225, 345)
-POST1_CENTER = (250, 345)
+from colors import COLOR_TABLE
 
 
 class Contour(object):
+    def __init__(self, points=None, color='black'):
+        self.points = points
+        self.color = color
+
+
+class ContourSet(object):
     def __init__(self, contours=None, bounding_box=None, index=0):
         self.contours = contours
         self.bounding_box = bounding_box
         self.index = index
 
 
-class ContourBag(object):
-    def __init__(self, contours=None):
-        self.contours = contours
+class ContourSetCollection(object):
+    def __init__(self, contour_sets=None):
+        self.contour_sets = contour_sets
 
         sort_key = lambda x: x.bounding_box[2] * x.bounding_box[3]
-        self.contours.sort(key=sort_key, reverse=True)
+        self.contour_sets.sort(key=sort_key, reverse=True)
 
     @property
     def has_contours(self):
-        return bool(self.contours)
+        return bool(self.contour_sets)
 
     def get_contour_less_than_width(self, width):
-        for i, cntr in enumerate(self.contours):
-            if cntr.bounding_box[3] < width:
+        for i, cntr_set in enumerate(self.contour_sets):
+            if cntr_set.bounding_box[3] < width:
                 break
         else:
             return None
 
-        return self.contours.pop(i)
+        return self.contour_sets.pop(i)
+
+
+def box_area(box):
+    return box[2] * box[3]
 
 
 def box_to_line_points(box):
@@ -78,18 +55,6 @@ def box_to_line_points(box):
     xs = (x, x + w, x + w, x, x)
     ys = (y, y, y + h, y + h, y)
     return xs, ys
-
-
-def build_post_contours(radius=POST_RADIUS):
-    radius /= PIXEL_SPACING
-    contours = []
-    for center in (POST0_CENTER, POST1_CENTER):
-        cy, cx = center
-        thetas = np.linspace(0, 2*np.pi, 15)
-        xs = np.cos(thetas)*radius + cx
-        ys = np.sin(thetas)*radius + cy
-        contours.append(pixels_to_points(np.vstack((xs, ys)).T))
-    return contours
 
 
 def contour_bounding_box(contour, offset=0.0):
@@ -105,65 +70,71 @@ def contour_centroid(contour):
     return xs.mean(), ys.mean()
 
 
-def create_graphics_context(index):
-    canvas = Canvas(filename="laser{}.pdf".format(index),
-                    pagesize=(CANVAS_W, CANVAS_H))
+def create_graphics_context(index, params):
+    pagesize = (params['output_width'] * params['output_dpi'],
+                params['output_height'] * params['output_dpi'])
+    canvas = Canvas(filename="laser{}.pdf".format(index), pagesize=pagesize)
+
     gc = PdfGraphicsContext(canvas)
-    gc.set_line_width(LASER_HAIRLINE)
-    gc.set_stroke_color((0.0, 1.0, 0.0))
+    gc.set_line_width(params['hairline_width'] * inch)
+
+    default_color = COLOR_TABLE[params['default_color']]
+    gc.set_stroke_color(default_color)
+    gc.set_fill_color(default_color)
 
     return gc
 
 
-def draw_contours(contour_bag):
+def draw_contours(contour_collection, params):
     context_count = 0
-    gc = create_graphics_context(context_count)
+    gc = create_graphics_context(context_count, params)
 
+    output_w = params['output_width'] * params['output_dpi']
+    output_h = params['output_height'] * params['output_dpi']
     origin_x, origin_y = 0.0, 0.0
     line_height = 0.0
 
-    while contour_bag.has_contours:
-        remaining_width = CANVAS_W - origin_x
-        slc_cntr = contour_bag.get_contour_less_than_width(remaining_width)
+    next_contour_set = contour_collection.get_contour_less_than_width
+
+    while contour_collection.has_contours:
+        remaining_width = output_w - origin_x
+        contour_set = next_contour_set(remaining_width)
 
         # Start a new row/PDF as needed
-        if slc_cntr is None:
+        if contour_set is None:
             origin_y += line_height
             origin_x = line_height = 0.0
 
-            if (origin_y + 2.75*PDF_DPI) > CANVAS_H:
+            if (origin_y + 2.75 * params['output_dpi']) > output_h:
                 # Start a new file
                 origin_y = 0.0
                 context_count += 1
                 gc.save()
-                gc = create_graphics_context(context_count)
+                gc = create_graphics_context(context_count, params)
+                #gc.begin_page()
             continue
 
-        contours = slc_cntr.contours
-        x, y, height, width = slc_cntr.bounding_box
+        contours = contour_set.contours
+        x, y, height, width = contour_set.bounding_box
 
         # Trace the contours
         with gc:
             gc.translate_ctm(origin_x, origin_y)
-            for i, cntr in enumerate(contours):
-                cntr[:, 1] -= x
-                cntr[:, 0] -= y
+            for cntr in contours:
+                points = cntr.points
+                points[:, 1] -= x
+                points[:, 0] -= y
                 with gc:
-                    # Everything after the first 3 contours is bone and should
-                    # be black so that the laser can etch it first.
-                    if i >= 3:
-                        gc.set_stroke_color((0.0, 0.0, 0.0))
-                    gc.lines(cntr)
+                    gc.set_stroke_color(COLOR_TABLE[cntr.color])
+                    gc.lines(points)
                     gc.close_path()
                     gc.stroke_path()
 
-        # Add a slice index in red
+        # Add a slice index
         with gc:
-            gc.set_stroke_color((1.0, 0.0, 0.0))
-            gc.set_fill_color((1.0, 0.0, 0.0))
             pos_x = origin_x + width / 2.
             pos_y = origin_y + height / 2.
-            gc.show_text_at_point(str(slc_cntr.index), pos_x, pos_y)
+            gc.show_text_at_point(str(contour_set.index), pos_x, pos_y)
 
         line_height = max(height, line_height)
         origin_x += width
@@ -171,44 +142,41 @@ def draw_contours(contour_bag):
     gc.save()
 
 
-def get_all_slice_contours(volume):
-    all_contours = []
-    last_slice = PARAMS['skin']['last_slice']
-    for i, slc in enumerate(volume[:last_slice]):
-        contours = get_slice_contours(slc, i)
-        bbox_padding = PDF_DPI * 0.125
-        bbox = contour_bounding_box(contours[0], offset=bbox_padding)
-        cntr = Contour(contours=contours, index=i, bounding_box=bbox)
-        all_contours.append(cntr)
+def get_all_slice_contours(volume, params):
+    bbox_padding = params['output_dpi'] * params['contour_bbox_padding']
+    bbox_index = params['contour_bbox_index']
+    contour_sets = []
+    for i, slc in enumerate(volume):
+        contours = get_slice_contours(slc, i, params)
+        if contours:
+            bbox = contour_bounding_box(contours[bbox_index].points,
+                                        offset=bbox_padding)
+            cntr = ContourSet(contours=contours, index=i, bounding_box=bbox)
+            contour_sets.append(cntr)
 
-    return ContourBag(contours=all_contours)
+    return ContourSetCollection(contour_sets=contour_sets)
 
 
-def get_slice_contours(slice, slice_index):
-    post_contours = build_post_contours()
+def get_slice_contours(slice, slice_index, params):
     slice_contours = []
-    for tissue in ('skin', 'bone'):
-        tissue_params = PARAMS[tissue]
-        contours = measure.find_contours(slice, tissue_params['isovalue'])
-        contour_index = tissue_params['contour_index'][slice_index]
-        if contour_index is not None:
-            # We know these are skin contours
-            cntr = pixels_to_points(contours[contour_index])
-            slice_contours.append(cntr)
-            slice_contours.extend(post_contours)
-        else:
-            # Only add bone contours if they don't maximally intersect
-            # the pre-defined bounding boxes of the CT scanner machine
-            for cntr in contours:
-                bbox = contour_bounding_box(cntr)
-                area = box_area(bbox)
-                for mach_box in (MACHINE_BOX_0, MACHINE_BOX_1):
-                    int_area = intersection_area(bbox, mach_box)
-                    if int_area > (0.95 * area):
-                        break
-                else:
-                    cntr = pixels_to_points(cntr)
-                    slice_contours.append(cntr)
+    clip_boxes = params['clip_boxes']
+    clip_overlap_percentage = params['clip_overlap_percentage']
+    for contour_params in params['contours']:
+        if slice_index > contour_params['last_slice']:
+            break
+        contours = measure.find_contours(slice, contour_params['isovalue'])
+        # Only add contours if they don't maximally intersect the clip boxes
+        for cntr in contours:
+            bbox = contour_bounding_box(cntr)
+            max_overlap_area = clip_overlap_percentage * box_area(bbox)
+            for clip_box in clip_boxes:
+                int_area = intersection_area(bbox, clip_box)
+                if int_area > max_overlap_area:
+                    break
+            else:
+                cntr_pnts = pixels_to_points(cntr, params)
+                cntr = Contour(points=cntr_pnts, color=contour_params['color'])
+                slice_contours.append(cntr)
 
     return slice_contours
 
@@ -226,6 +194,21 @@ def intersection_area(box0, box1):
     return overlap(x00, x10, x01, x11) * overlap(y00, y10, y01, y11)
 
 
+def pixels_to_points(contour, params):
+    scale_factor = params['scale_factor']
+    voxel_dims = params['voxel_dims']
+    x_dims, y_dims = voxel_dims[:2]
+    x_factor = x_dims * mm * scale_factor
+    y_factor = y_dims * mm * scale_factor
+    return contour * np.array([y_factor, x_factor])
+
+
+def read_params(filename):
+    with open(filename, 'r') as fp:
+        params = json.load(fp)
+    return params
+
+
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -235,19 +218,23 @@ def main():
     parser.add_argument('-n', '--node-path', default='/ct',
                         help='The path to the node in the HDF5 file '
                              'containing the volume.')
+    parser.add_argument('-p', '--params',
+                        help='The path to the parameters file')
 
     args = parser.parse_args()
+
+    params = read_params(args.params)
 
     h5 = tables.openFile(args.input_file)
     volume = h5.getNode(args.node_path)[:]
     h5.close()
 
     vol_max = volume.max()
-    for parms in PARAMS.values():
-        parms['isovalue'] *= vol_max
+    for contour in params['contours']:
+        contour['isovalue'] *= vol_max
 
-    contour_bag = get_all_slice_contours(volume)
-    draw_contours(contour_bag)
+    contour_collection = get_all_slice_contours(volume, params)
+    draw_contours(contour_collection, params)
 
 
 if __name__ == '__main__':
